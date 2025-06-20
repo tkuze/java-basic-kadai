5415fa0f2014e626f5ccf9b2b4cbdaa1d955fe45
tkuze <tkuze@aicom.co.jp>
2025-06-20 14:03:23 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,25 @@
+package kadai_018;
+
+abstract class Kato_Chapter18 {
+
+	public String familyName = "加藤";
+	public String givenName = "";
+	public String address = "東京都中野区○×";
+	
+	public void commonIntroduce() {
+		
+		System.out.println("名前は" + this.familyName + this.givenName + "です");
+		
+		System.out.println("住所は" + this.address + "です");
+		
+	};
+	
+	abstract void eachIntroduce();
+	
+	public void execIntroduce() {
+		
+		commonIntroduce();
+		eachIntroduce();
+		
+	}
+}
