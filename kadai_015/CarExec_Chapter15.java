fd1e736fe961639f98a8ed5f419fbbda0b4738f4
tkuze <tkuze@aicom.co.jp>
2025-06-20 10:35:02 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,33 +1,17 @@
 package kadai_015;
 
 public class CarExec_Chapter15 {
-	private int gear = 1;
-	private int speed = 10;
 	
-	
-	public CarExec_Chapter15(int gear, int speed) {
-		this.gear = gear;
-		this.speed = speed;
-	}
-	
-	
-	public void GearChange(int afterGear) {
+
+	public static void main(String[] args) {
 		
-		switch(afterGear) {
+		Car_Chapter15 car = new Car_Chapter15 ();
 		
-		case 1 	-> speed = 10;
-		case 2 	-> speed = 20;
-		case 3 	-> speed = 30;
-		case 4 	-> speed = 40;
-		case 5 	-> speed = 50;
-		default -> speed = 10;
-	
-		}
-	}
-	
-	public void run() {
+		car.gearChange(3);
 		
-		System.out.println(speed);
+		car.run();
+	
 	}
+
 
 }
